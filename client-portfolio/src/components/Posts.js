f078edfb83a4ee30ea7025c5ce833388a438feb7
rexaevum <rolans2k@gmail.com>
2021-02-26 17:20:01 -0500

    updated formatting for posts page

--- client-portfolio/src/components/Posts.js
+++ client-portfolio/src/components/Posts.js
@@ -24,7 +24,6 @@ const Posts = props => {
         .then( data => setPostData(data)).catch(console.error)
     }, []);
 
-    console.log(postData)
     return (
         <main className="bg-blue-400 min-h-screen p-12">
             <section className="container mx-auto">
@@ -34,12 +33,12 @@ const Posts = props => {
                 <h2 className="text-lg text-grey-600 flex justify-center mb-12">
                     Check out what's new
                 </h2>
-                <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
                     {postData && postData.map( (post, index) => (
                         <article>
                             <Link to={`/post/${post.slug.current}`} key={post.slug.current}>
                                 <span className="block h-64 relative rounded shadow leading-snug bg-blue-50 border-l-8 border-green-400" key={index}>
-                                    <img src={post.mainImage && post.mainImage.asset.url || stock} alt={post.mainImage && post.mainImage.alt} className="w-full h-full rounded-r object-cover absolute"  />
+                                    <img src={post.mainImage && post.mainImage.asset.url || stock} alt={post.mainImage && post.mainImage.alt} className="w-full h-full rounded-r object-cover absolute inline"  />
                                     <span className="block relative h-full flex justify-end items-end pr-4 pb-4">
                                         <h3 className="text-grey-800 text-lg font-blog px-3 py-4 bg-red-700 text-red-100 rounded">{post.title}</h3>
                                     </span>
@@ -53,8 +52,5 @@ const Posts = props => {
     )
 }
 
-Posts.propTypes = {
 
-}
-
-export default Posts
+export default Posts;
