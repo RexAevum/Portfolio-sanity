b469c465baff8f94779a4e59b47059c70529d816
rexaevum <rolans2k@gmail.com>
2021-02-26 15:45:22 -0500

    updated formatting for navbar on moibile devices

--- client-portfolio/src/components/Navbar.js
+++ client-portfolio/src/components/Navbar.js
@@ -10,9 +10,9 @@ const Navbar = () => {
 
     return (
         <header className="bg-blue-800">
-            <div className="container mx-auto flex justify-between">
-                <nav className="flex">
-                    <NavLink exact to='/' activeClassName="text-grey-50" className="inline-flex items-center py-5 px-3 mr-4 text-red-100 hover:text-red-800 text-4xl font-bold cursive tracking-widest" >
+            <div className="container sm:mx-auto flex justify-between">
+                <nav className="flex flex-wrap">
+                    <NavLink exact to='/' activeClassName="text-grey-50" className="inline-flex items-center py-1 px-3 sm:py-5 sm:px-3 sm:mr-4 text-red-100 hover:text-red-800 text-4xl font-bold cursive tracking-widest" >
                         Rolans A
                     </NavLink>
                     <NavLink to='/about' activeClassName="text-red-100 bg-blue-500 rounded" className="inline-flex items-center py-3 px-3 my-6 text-red-200 hover:text-red-800 ">
@@ -25,10 +25,10 @@ const Navbar = () => {
                         My Posts
                     </NavLink>
                 </nav>
-                <div className="inline-flex py-3 px-3 my-6">
-                    <SocialIcon url={myMail} nework="email" label="DevConnector" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35, width: 35}}/>
-                    <SocialIcon url={LinkedIn} className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35, width: 35}}  />
-                    <SocialIcon url={Github} className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35, width: 35}}/>
+                <div className="block flex-wrap py-3 px-3 my-6 bg-blue-800">
+                    <SocialIcon url={myMail} nework="email" label="DevConnector" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35, width: 35}} className="inline-flex items-center mb-1 md:mx-2"/>
+                    <SocialIcon url={LinkedIn} className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35, width: 35}} className="inline-flex items-center mb-2 md:mx-2" />
+                    <SocialIcon url={Github} className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35, width: 35}} className="inline-flex items-center md:mx-2 sm:mb-2"/>
                 </div>
             </div>
         </header>
